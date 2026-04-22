fd2b73a1e65d5788a91f7fecff5797133785cff4
marcjason2012 <marcjete12@gmail.com>
2026-04-22 10:47:52 -0700

    Update SudokuMain.java (Remove Folder Access)

--- SudokuMain.java
+++ SudokuMain.java
@@ -1,6 +1,6 @@
 public class SudokuMain {
     public static void main(String[] args) {
-        SudokuBoard sudoku = new SudokuBoard("Data/data1.sdk");
+        SudokuBoard sudoku = new SudokuBoard("data1.sdk");
         System.out.println(sudoku);
     }
 }
